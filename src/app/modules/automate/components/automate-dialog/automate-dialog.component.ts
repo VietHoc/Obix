6f5dd1583b372df5660cb1d2678ef819f682b845
viethoc <hocle@novahub.vn>
2019-09-16 16:28:27 +0700

    add customvalidate

--- src/app/modules/automate/components/automate-dialog/automate-dialog.component.ts
+++ src/app/modules/automate/components/automate-dialog/automate-dialog.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Automate} from '../../../../shared/models/automate';
+import {CustomValidators} from '../../../../shared/validators/custom-validators';
 
 @Component({
   selector: 'app-automate-dialog',
@@ -23,7 +24,7 @@ export class AutomateDialogComponent implements OnInit {
   private buildForm() {
     this.formServer = this.formBuilder.group({
       name: [this.automate.name, [Validators.required]],
-      ip: [this.automate.ip, [Validators.required]],
+      ip: [this.automate.ip, [Validators.required, CustomValidators.ip]],
       uri: [this.automate.uri, [Validators.required]],
       active: [this.automate.active, [Validators.required]]
     });
---
+++ src/app/shared/validators/custom-validators.ts
@@ -0,0 +1,24 @@
+import { ValidationErrors, AbstractControl } from '@angular/forms';
+
+export class CustomValidators {
+  private static regexEmail = /^[\w!#$%&'*+-/=?^_`{|}~()]+@[\w!#$%&'*+-/=?^_`{|}~()]+(\.[\w!#$%&'*+-/=?^_`{|}~()]+)+$/;
+  private static regexIp = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/;
+
+
+  static email(control: AbstractControl): ValidationErrors | null {
+    const email = control.value as string;
+    if (!!email && !CustomValidators.regexEmail.test(email)) {
+        return { email: true };
+      }
+    return null;
+  }
+
+  static ip(control: AbstractControl): ValidationErrors | null {
+    const ip = control.value as string;
+    if (!!ip && !CustomValidators.regexIp.test(ip)) {
+        return { ip: true };
+    }
+    return null;
+  }
+}
+
