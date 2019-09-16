4c1fcc469020148f45304cca1e8e10c77880f836
viethoc <hocle@novahub.vn>
2019-09-16 16:49:34 +0700

    change icon active automate

--- src/app/shared/validators/custom-validators.ts
+++ src/app/shared/validators/custom-validators.ts
@@ -2,7 +2,7 @@ import { ValidationErrors, AbstractControl } from '@angular/forms';
 
 export class CustomValidators {
   private static regexEmail = /^[\w!#$%&'*+-/=?^_`{|}~()]+@[\w!#$%&'*+-/=?^_`{|}~()]+(\.[\w!#$%&'*+-/=?^_`{|}~()]+)+$/;
-  private static regexIp = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/;
+  private static regexIp = /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/;
 
 
   static email(control: AbstractControl): ValidationErrors | null {
