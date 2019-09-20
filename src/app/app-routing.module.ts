c0cb9e1632866ca28b2d789c81bb99446cbe95a9
viethoc <hocle@novahub.vn>
2019-09-20 15:02:49 +0700

    Add cmd build prod to package.js

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {SensorListComponent} from './modules/sensors/pages/sensor-list.component';
-
 
 const routes: Routes = [
   {
