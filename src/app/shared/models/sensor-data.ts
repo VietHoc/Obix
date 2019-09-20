e0bfc0c07ad43045641256f2a0c959fed6fa6132
viethoc <hocle@novahub.vn>
2019-09-20 15:27:44 +0700

    Add icon to type bool of sensor

--- src/app/shared/models/sensor-data.ts
+++ src/app/shared/models/sensor-data.ts
@@ -2,6 +2,8 @@ export class SensorData {
   id: number;
   sensorId: number;
   sensortypeId: number;
+  sensortypeName: string;
+  sensortypeType: string;
   name: string;
   value: string;
   locationIdentifier: string;
