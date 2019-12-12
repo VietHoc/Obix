31ad357db2f0e371fff230b5954069498b3bc0d1
hocle@novahub.vn <>
2019-12-12 10:39:50 +0700

    add fake data

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {SharedModule} from '../shared/shared.module';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {HttpErrorInterceptor} from './interceptor/error-handler.interceptor';
+import { FakeBackendProvider } from './http/fake-backend-interceptor.service';
 
 @NgModule({
   declarations: [],
@@ -13,6 +14,7 @@ import {HttpErrorInterceptor} from './interceptor/error-handler.interceptor';
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
+    FakeBackendProvider,
   ]
 })
 export class CoreModule { }
